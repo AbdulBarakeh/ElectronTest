3960e0e060702f599cfb2c1c55df869554697ae2
Abdul-Rahman Abdurrohman <abdul.abdurrohman@hotmail.com>
2021-02-24 00:58:42 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron')
+
+const env = process.env.NODE_ENV || 'development'; 
+
+// If development environment 
+if (env === 'development') { 
+	try { 
+		require('electron-reloader')(module, { 
+			debug: true, 
+			watchRenderer: true
+		}); 
+	} catch (_) { console.log('Error'); }	 
+} 
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+
+  ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light'
+    } else {
+      nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+  })
+
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+  })
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
